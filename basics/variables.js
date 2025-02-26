169bbccc37ce2341f6134b3fba808931cb7bfd3d
Sai Krishna Vinjamuri <146044443+saikrishna-vinjamuri@users.noreply.github.com>
2025-02-26 11:40:35 +0000

    basics commit_1

---
+++ basics/variables.js
@@ -0,0 +1,26 @@
+  
+
+// this is my first java script practice
+
+// Declaring variables
+
+/* 1) Declaring const
+   2) Declaring let
+   3) Declaring var
+   4) Declaring normal memory name
+*/
+
+
+const customer_id = 123456
+let   customer_name = "kumar"
+let   customer_city
+let   customer_gmail = 'random@gmail.com'
+
+
+console.table([customer_city,customer_gmail,customer_id,customer_name])
+
+customer_city = 'WARANGAL'
+customer_name = 'Ramesh'
+customer_gmail = 'random1@gmail.com'
+
+console.table([customer_city,customer_gmail,customer_id,customer_name])
